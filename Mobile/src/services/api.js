8409d112404c62e7f97e099527eaff0e7e534cc7
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-05-07 18:25:27 -0300

    Modificando rota da API

--- Mobile/src/services/api.js
+++ Mobile/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const hostAPICarnaiba = 'http://10.0.0.195:3000'
+const hostAPISerraTalhada = 'http://192.168.1.4:3000'
+
 const api = axios.create({
-  baseURL: 'http://192.168.1.4:3000',
+  baseURL: hostAPICarnaiba,
 
 });
 
