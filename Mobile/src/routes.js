c19804ef3615cf1f48d3eb7fbd2cb094c9b275f3
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-05-08 12:43:55 -0300

    Adicionando Rotas Aninhadas

--- Mobile/src/routes.js
+++ Mobile/src/routes.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { 
-  StyleSheet, 
-  Image 
+import {
+  StyleSheet,
+  Image
 } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
@@ -22,18 +22,36 @@ import LinksUteis from './Pages/LinksUteis';
 import Materias from './Pages/Materias';
 import Metas from './Pages/Metas';
 
+import AddLembrete from './Pages/AddLembrete';
+
 //import { Agenda } from 'react-native-calendars';
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
-export default function Routes() {
+function ScreenAddLembrete() {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen 
+        name="Add Lembrete"
+        component={AddLembrete}
+        options={{
+          headerShown:false
+        }}
+      />
+
+    </Stack.Navigator>
+  );
+}
+
+function ScreensMainTab() {
   return (
     <Tab.Navigator
       initialRouteName='Agenda'
       screenOptions={{
         tabBarActiveTintColor: 'rgba(102,255,240,1)',
         tabBarInactiveTintColor: 'rgba(255,255,255,255)',
-        
+
         tabBarStyle: {
           paddingTop: 2,
           paddingLeft: 5,
@@ -48,7 +66,7 @@ export default function Routes() {
         headerStyle: {
           height: 80
         },
-        
+
         headerShown: false
       }}
     >
@@ -56,7 +74,7 @@ export default function Routes() {
         name="Materias"
         component={Materias}
         options={{
-          tabBarIcon:({focused ,size, color}) => (
+          tabBarIcon: ({ focused, size, color }) => (
             <Image style={styles.logo} source={materias} />
           )
         }}
@@ -66,7 +84,7 @@ export default function Routes() {
         name="Lembretes"
         component={Lembretes}
         options={{
-          tabBarIcon:({focused, size, color}) => (
+          tabBarIcon: ({ focused, size, color }) => (
             <Image style={styles.logo} source={lembretes} />
           )
         }}
@@ -77,7 +95,7 @@ export default function Routes() {
         component={Agenda}
         options={{
           tabBarLabel: '',
-          tabBarIcon:({focused, size, color}) => (
+          tabBarIcon: ({ focused, size, color }) => (
             <ButtonCalendar />
           ),
           tabBarBadge: 0
@@ -88,7 +106,7 @@ export default function Routes() {
         name="Links"
         component={LinksUteis}
         options={{
-          tabBarIcon:({focused, size, color}) => (
+          tabBarIcon: ({ focused, size, color }) => (
             <Image style={styles.logo} source={linksUteis} />
           )
         }}
@@ -98,13 +116,36 @@ export default function Routes() {
         name="Metas"
         component={Metas}
         options={{
-          tabBarIcon:({focused, size, color}) => (
+          tabBarIcon: ({ focused, size, color }) => (
             <Image style={styles.logo} source={metas} />
           )
         }}
       />
     </Tab.Navigator>
-  )
+  );
+}
+
+export default function Routes() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen 
+        name="Screens Main"
+        component={ScreensMainTab}
+        options={{
+          headerShown: false
+        }}
+      />
+
+      <Stack.Screen 
+        name="Screen Stack Add Lembrete"
+        component={ScreenAddLembrete}
+        options={{
+          headerShown: false
+        }}
+      />
+
+    </Stack.Navigator>
+  );
 }
 
 const styles = StyleSheet.create({
