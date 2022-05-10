d6e03dcd667a89e22c3eca28e924f6e3a505818c
LucasGabryellll <gabryelmonteiro1@gmail.com>
2022-05-10 20:55:44 -0300

    Configurando Header nas requisições

--- Mobile/src/services/api.js
+++ Mobile/src/services/api.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
+
 const hostAPICarnaiba = 'http://10.0.0.195:3000'
 const hostAPISerraTalhada = 'http://192.168.1.4:3000'
 
 const api = axios.create({
-  baseURL: hostAPICarnaiba,
-
+  baseURL: hostAPISerraTalhada,
+  
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
 });
 
 export default api;
